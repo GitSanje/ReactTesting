957ec4cf972cc2096045d6731f735756488118a0
Sanjay <karkisanjay2002@gmail.com>
2025-12-05 15:05:20 +0545

    providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Application } from './components/application/Application'
+import { CounterTwo } from './components/counter-two/CounterTwo'
+import { Counter } from './components/counter/Counter'
+import { MuiMode } from './components/mui/MuiMode'
+import { Skills } from './components/skills/Skills'
+import { Users } from './components/users/Users'
+import { AppProviders } from './providers/AppProviders'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AppProviders>
+      <div className="App">
+        <Application />
+        <Skills skills={['HTML', 'CSS']} />
+        <Counter />
+        <CounterTwo count={1} />
+        <Users />
+        <MuiMode />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </AppProviders>
   )
 }
 
