ed11d41fe4174c01185be670f938ccf219c3095d
Sanjay <karkisanjay2002@gmail.com>
2025-12-05 14:34:21 +0545

    debug

--- src/components/skills/Skills.test.tsx
+++ src/components/skills/Skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen ,logRoles} from '@testing-library/react'
 import { Skills } from './Skills'
 import { describe,test,expect } from 'vitest'
 
@@ -31,7 +31,9 @@ describe('Skills', () => {
   })
 
   test('Start Learning button is eventually displayed', async () => {
-    render(<Skills skills={skills} />)
+      const   view =  render(<Skills skills={skills} />)
+      logRoles(view.container)
+    screen.debug()
     const startLearningButton = await screen.findByRole(
       'button',
       {
@@ -41,6 +43,8 @@ describe('Skills', () => {
         timeout: 1002,
       }
     )
+    screen.debug()
+
     expect(startLearningButton).toBeInTheDocument()
   })
 })
